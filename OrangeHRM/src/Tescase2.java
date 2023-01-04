b45abf719eac414e7c9c298dd416592770ee0f6e
kashi <kashireddyerugula@gmail.com>
2023-01-04 16:01:46 +0530

    c2 is updated

--- OrangeHRM/src/Tescase2.java
+++ OrangeHRM/src/Tescase2.java
@@ -4,6 +4,7 @@ public class Tescase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is cs2");
+		System.out.println("Test2 is updated");
 	}
 
 }
